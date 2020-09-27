a3799ea134515a0c614fe48fdf8633a1929ff850
Vadim Firsov <49113704+vadfirsov@users.noreply.github.com>
2020-09-27 12:27:30 +0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,143 @@
+/*eslint-env browser*/
+
+//---------- NAVIGATION --------------
+
+function didTapAboutMe() {
+    window.scrollTo({
+        top: document.getElementById('container-about-me').offsetTop,
+        left: 0,
+        behavior: 'smooth'
+    });
+    closeDropdown();
+}
+
+function didTapProjects() {
+    window.scrollTo({
+        top: document.getElementById('container-projects').offsetTop,
+        left: 0,
+        behavior: 'smooth'
+    });
+    closeDropdown();
+}
+
+function didTapContact() {
+    window.scrollTo({
+        top: document.getElementById('container-contact-me').offsetTop,
+        left: 0,
+        behavior: 'smooth'
+    });
+    closeDropdown();
+}
+
+//---------- DROP DOWN --------------
+var topNav = document.getElementById('dropdownClick');
+var dropdown = document.getElementById('dropdownIcon');
+
+dropdown.addEventListener("click", function () {
+    if (topNav.className === "topnav") {
+        topNav.className += " responsive";
+        topNav.style.height = "200px";
+    } else {
+        closeDropdown();
+    }
+});
+
+function closeDropdown() {
+    topNav.className = "topnav";
+    topNav.style.height = "50px";
+}
+
+//---------- SLIDER --------------
+function moveSlider(innerSliderName, imgCount, isLeft) {
+
+    var innerSlider = document.getElementById(innerSliderName)
+
+    var currentMargin = parseFloat(innerSlider.style.marginLeft);
+    if (!currentMargin) {
+        currentMargin = 0;
+    }
+
+    var additionalMargin = -100;
+    if (isLeft == true) {
+        additionalMargin *= -1;
+    }
+    var newMargin = (currentMargin + additionalMargin) + "%";
+    //the last highest margin
+    var lastMargin = ((imgCount - 1) * -100);
+
+    //checks if should move to beginning/end
+    if (isLeft && currentMargin >= 0) {
+        newMargin = lastMargin + "%";
+    } else if (!isLeft && currentMargin <= lastMargin) {
+        newMargin = "0%";
+    }
+    innerSlider.style.marginLeft = newMargin;
+
+    var selectedIndex = -1 * parseFloat(newMargin) / 100;
+    var sliderNum = innerSliderName.substr(innerSliderName.length - 1)
+    var bullets = document.getElementsByClassName('bullet' + sliderNum)
+    for (i = 0; i < bullets.length; i++) {
+        bullets[i].style.backgroundColor = "#c5c5d1";
+        if (i == selectedIndex) {
+            bullets[i].style.backgroundColor = "#292939";
+        }
+    }
+}
+
+function setBulletChecked(innerSlider, imgCount, isLeft) {
+
+}
+
+//---------- DROP DOWN --------------
+//---------- DROP DOWN --------------
+
+
+//---------- CONTACT LIST --------------
+var tfName = document.getElementById('name');
+var tfEmail = document.getElementById('email');
+var tfMessage = document.getElementById('message');
+
+var errName = document.getElementById('errName');
+var errEmail = document.getElementById('errEmail');
+var errMsg = document.getElementById('errMsg');
+
+function didClickSend() {
+    var isValid = validateTextFields();
+    if (isValid) {
+        //send email
+    }
+}
+
+function validateTextFields() {
+    var isValid = true;
+    if (!tfName.value || tfName.value == "") {
+        errName.textContent = "Name can't be empty";
+        errName.style.visibility = "visible";
+        isValid = false;
+    }
+
+    var atPosition = tfEmail.value.indexOf("@");
+    var dotPosition = tfEmail.value.lastIndexOf(".");
+    var isDotInRightPosition = dotPosition + 2 >= tfEmail.value.length;
+    if (!tfEmail.value || tfEmail.value == "") {
+        errEmail.textContent = "Email can't be empty";
+        errEmail.style.visibility = "visible";
+        isValid = false;
+    } else if (atPosition < 1 || dotPosition < atPosition + 2 || isDotInRightPosition) {
+        errEmail.textContent = "The Email is not valid";
+        errEmail.style.visibility = "visible";
+        isValid = false;
+    }
+    if (!tfMessage || tfMessage.value == "") {
+        errMsg.textContent = "Message can't be empty";
+        errMsg.style.visibility = "visible";
+        isValid = false;
+    }
+    return isValid;
+}
+
+function resetErrors() {
+    errName.style.visibility = "hidden";
+    errEmail.style.visibility = "hidden";
+    errMsg.style.visibility = "hidden";
+}
